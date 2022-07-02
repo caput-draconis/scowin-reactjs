2b374899a07f913da15588fddbfe62a2a652751e
caput-draconis <2021cfse014@wilp.bits-pilani.ac.in>
2022-07-02 20:29:10 +0530

    Add integration test

--- features/stepDefinitions/dashboard.js
+++ features/stepDefinitions/dashboard.js
@@ -1,24 +1,21 @@
 const { Given, Then } = require('cucumber')
-const {expect} = require('expect');
+const { expect } = require('expect');
 const { By } = require('selenium-webdriver');
 
 require('chromedriver');
 
 Given('{string} element', { timeout: 60000 }, async function (element) {
-    switch(element){
+    switch (element) {
         case 'root':
-            this.element = await this.driver.findElement(By.id('root'));
+            this.element = await this.driver.findElement(By.xpath('//div[@class="App"]'));
             break;
         default:
             throw new Error('element not defined');
     }
-
-
-
 });
 
 
 Then('the element is identified in the page', { timeout: 60000 }, async function () {
-    let isDisplayed = await this.driver.findElement(this.element).isDisplayed();
+    let isDisplayed = await this.element.isDisplayed()
     expect(isDisplayed).toBeTruthy();
 });
--- features/stepDefinitions/hooks/before.js
+++ features/stepDefinitions/hooks/before.js
@@ -1,13 +1,14 @@
 const { Before } = require("cucumber");
-const {Builder} = require("selenium-webdriver");
+const { Builder, until, By } = require("selenium-webdriver");
 const baseURL = require('../../../src/testEnvironment')
 
-Before({timeout: 60000},async function(){
+Before({ timeout: 60000 }, async function () {
     this.URL = `${baseURL()}/scowin-reactjs/`
     try {
         this.driver = await new Builder().forBrowser('chrome').build();
         await this.driver.get(this.URL);
         await this.driver.manage().window().maximize();
+        await this.driver.wait(until.elementIsVisible(await this.driver.findElement(By.xpath('//div[@class="App"]'))))
     }
     catch (exception) {
         console.log("Unable to start driver ", exception);
